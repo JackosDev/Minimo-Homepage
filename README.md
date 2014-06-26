3e276cf2d25d4e64537ca27b98645ffaf265e6f9
JackosDev <jackos.dev@gmail.com>
2014-06-26 22:01:42 +0200

    More configuration options

--- README.md
+++ README.md
@@ -4,32 +4,20 @@ A simple homepage/startpage alternative made with HTML + CSS. [[Live Demo](http:
 
 **Important:** This page should work without problems in Firefox 28.0, Chrome 29.0, Opera 17.0 and IE 11.0.
 
-Customization
--------------
-| File                  | Info |
-|-----------------------|------|
-| `index.html`          | Used in the [live demo](http://jackosdev.github.io/Minimo-Homepage/). |
-| `home.html`           | [Example](http://jackosdev.github.io/Minimo-Homepage/home.html) used in the live demo. |
-| `css/theme.css`, `css/theme-dark.css` | Example themes. Use these as a base for your own. |
-| `css/main.css`        | There's not much to edit here, mainly alignements. |
-| `css/main-webkit.css` | For old browsers, a complement for `main.css`.  |
-| `js/settings.js`        | Settings. | 
 
-Edit `settings.js` as you like: 
-```javascript
-// Mascots
-var mascotEnable = false;
-var mascotUrl = "images/mascots/mascot1.png";
-var mascotPosition = "bottom left";
+Features
+--------
+An awesome configuration file where you can set (without the need to edit the HTML file):
+* Mascots! (aka. Waifus)
+* Custom theme
+* Day/Night theme depending on the hour of the day
+* Page title
+* Navigation links
 
-// Theme
-var themeUrl = "css/theme.css";
-```
-Use `mascotPosition = ""` to use the position in `theme.css`. Same for `mascotUrl`.
-To use your own mascot, set an URL.
 
 To Do
 -----
 * Clean ToDo list. **Done**.
 * Keep `main-webkit.css` updated.
+* Update Readme.
 * ?
--- js/main.js
+++ js/main.js
@@ -3,17 +3,26 @@ $(document).ready(function() {
         Do not edit here if you don't know what you're doing
     */
 
+    /* Header */
+    if (pageTitle != "") {
+        var headerBlock = document.getElementsByTagName("header")[0];
+        headerBlock.getElementsByTagName("h1")[0].innerHTML = pageTitle;
+    }
+
     /* Mascots */
     if (mascotEnable) {
+        // Create mascot div
+        var mascotBlock = document.createElement("div");
+        mascotBlock.setAttribute("id", "mascot");
+        document.body.appendChild(mascotBlock);
+
         $("#mascot").css("background-position", mascotPosition);
+        $("#mascot").css("background-size", mascotSize);
 
         if (mascotUrl == "")
             $("#mascot").css("background-image", "");
         else
             $("#mascot").css("background-image", "url(" + mascotUrl + ")");
-
-    } else {
-        $("#mascot").css("background-image", "none");
     }
 
     /* Theme */
@@ -25,13 +34,39 @@ $(document).ready(function() {
     
     // Change theme by time
     if (themeTime) {
-        var time = new Date;
-        var timeHours = time.getHours();
+        var time = new Date().getHours();
 
-        if (timeHours >= themeTime_DayHour && timeHours < themeTime_NightHour)
+        if (time >= themeTime_DayHour && time < themeTime_NightHour)
             $("#theme").attr("href", themeTime_DayUrl);
         else
             $("#theme").attr("href", themeTime_NightUrl);
+    }
+    
+    /* Navigation */
+    if (navigationJs) {
+        var navBlock = document.getElementById("navigation");
+        
+        for (i = 0; i < navList.length; ++i) {
+            if (i == 0)
+                var navListContainer = document.createElement("ul");
+            else if (navList[i] == "---") {
+                var navListContainer = document.createElement("ul");
+                ++i;
+            }
+            
+            if (!(i % 2)) {
+                var navListItem = document.createElement("li");
+                
+                var navListItemContent = document.createElement("a");
+                navListItemContent.href = navList[i + 1];
+                navListItemContent.innerHTML = navList[i];
+                navListItem.appendChild(navListItemContent);
+                
+                navListContainer.appendChild(navListItem);
+            }
+
+            navBlock.appendChild(navListContainer);
+        }
     }
 });
 
